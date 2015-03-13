37daab597cdb928fdead7b0212903968a9216a35
peterevans <pevans@fwparker.org>
2015-03-13 13:35:52 -0500

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,18 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Peter Evans on 3/5/15.
+//  Copyright (c) 2015 Peter Evans. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    
+    //The RecordedAudio class holds the title and filepath of each voice recording from the Main View Controller
+    
+    var filePathUrl: NSURL!
+    var title: String!
+    
+}
